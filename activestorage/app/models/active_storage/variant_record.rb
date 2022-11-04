ca5d8062488dc2ddb055ac007c2776e3da18f877
James Youn <jyoun@godaddy.com>
2022-11-04 11:33:40 -0700

    define primary key column for activestorage variant records

--- activestorage/app/models/active_storage/variant_record.rb
+++ activestorage/app/models/active_storage/variant_record.rb
@@ -4,7 +4,7 @@ class ActiveStorage::VariantRecord < ActiveStorage::Record
   self.table_name = "active_storage_variant_records"
 
   alias_method :uuid, :id
+  belongs_to :blob, primary_key: :uuid
 
-  belongs_to :blob
   has_one_attached :image
 end
