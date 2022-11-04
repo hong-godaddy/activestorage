93e52bc2435039d6db1c35ed6d1565af3adb7f3f
James Youn <jyoun@godaddy.com>
2022-11-04 09:55:55 -0700

    alias method :uuid -> :id

--- activestorage/app/models/active_storage/variant_record.rb
+++ activestorage/app/models/active_storage/variant_record.rb
@@ -3,6 +3,8 @@
 class ActiveStorage::VariantRecord < ActiveStorage::Record
   self.table_name = "active_storage_variant_records"
 
+  alias_method :uuid, :id
+
   belongs_to :blob
   has_one_attached :image
 end
