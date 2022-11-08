87e8e9e1e7a642b121c46ca47ceca13e3316df9d
James Youn <jyoun@godaddy.com>
2022-11-08 12:11:25 -0800

    use #alias_attribute to map uuid calls to id

--- activestorage/app/models/active_storage/variant_record.rb
+++ activestorage/app/models/active_storage/variant_record.rb
@@ -3,7 +3,7 @@
 class ActiveStorage::VariantRecord < ActiveStorage::Record
   self.table_name = "active_storage_variant_records"
 
-  alias_method :uuid, :id
+  alias_attribute :uuid, :id
   belongs_to :blob, primary_key: :uuid
 
   has_one_attached :image
